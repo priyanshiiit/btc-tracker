e4236307c8d1fb5df568a19d84049fe155cb7ac0
priyanshiiit <priyanshiiit@gmail.com>
2024-03-14 15:41:34 +0530

    Integrates API

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ symbol, price }: { symbol: string; price: number }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 w-64">
-      <div className="flex flex-col">
-        <span className="text-gray-600 font-semibold mb-2">{symbol}</span>
-        <span className="text-xl font-bold">{price}</span>
+    <Link to={`/overview/${symbol}`}>
+      <div className="bg-white rounded-lg shadow-md p-6 w-64">
+        <div className="flex flex-col">
+          <span className="text-gray-600 font-semibold mb-2">{symbol}</span>
+          <span className="text-xl font-bold">{price}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 
 const data = [
@@ -8,10 +8,38 @@ const data = [
 ];
 
 const HomePage = () => {
+  const [prices, setPrices] = useState<{ [key: string]: number }>({
+    usd: 1000,
+    jpy: 10,
+    eur: 20,
+  });
+
+  const fetchBitcoinPrices = async () => {
+    try {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,jpy,eur"
+      );
+
+      if (!response.ok) {
+        alert("Failed to fetch Bitcoin prices");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setPrices(data.bitcoin);
+    } catch (error) {
+      console.error("Error fetching Bitcoin prices:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBitcoinPrices();
+  }, []);
+
   return (
     <div className="flex flex-wrap gap-4 justify-center mt-10">
-      {data.map((value: any, index: any) => {
-        return <Card key={index} symbol={value.sybmol} price={value.price} />;
+      {Object.entries(prices).map(([symbol, price], index: any) => {
+        return <Card key={index} symbol={symbol} price={price} />;
       })}
     </div>
   );
